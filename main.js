9ef93198e8f559f230888be245081be91ccef4a6
fabroche <kraxusmmo@gmail.com>
2024-04-02 17:14:39 -0400

    Updates

--- main.js
+++ main.js
@@ -1,52 +1,3 @@
-// let desglose = {
-//     billetes_10: 10,
-//     billetes_20: 50,
-//     billetes_50: 109,
-//     billetes_100: 500,
-//     billetes_200: 0,
-//     billetes_500: 37,
-//     billetes_1000: 8
-// }
-//
-// const get_desglose = () => {
-//
-//     const split = {
-//         '10': 10 * desglose.billetes_10,
-//         '20': 20 * desglose.billetes_20,
-//         '50': 50 * desglose.billetes_50,
-//         '100': 100 * desglose.billetes_100,
-//         '200': 200 * desglose.billetes_200,
-//         '500': 500 * desglose.billetes_500,
-//         '1000': 1000 * desglose.billetes_1000,
-//     }
-//     let total = 0
-//     for (let value in split) {
-//         total += split[value]
-//     }
-//     split['Total'] = total
-//     return split
-// }
-// const result = get_desglose()
-// console.log(result)
-// alert(
-//     `
-//     ======================= \n
-//     Desglose 🙂 \n
-//     ======================= \n
-//     10 x ${desglose.billetes_10} = ${result["10"]} \n
-//     20 x ${desglose.billetes_20} = ${result["20"]} \n
-//     50 x ${desglose.billetes_50} = ${result["50"]} \n
-//     100 x ${desglose.billetes_100} = ${result["100"]} \n
-//     200 x ${desglose.billetes_200} = ${result["200"]} \n
-//     500 x ${desglose.billetes_500} = ${result["500"]} \n
-//     1000 x ${desglose.billetes_1000} = ${result["1000"]} \n
-//     ======================= \n
-//     Total = ${result["Total"]} \n
-//     ======================= \n
-//     `
-// )
-
-
 function main() {
 
     // DOM elements
@@ -77,9 +28,6 @@ function main() {
 
     // Iniciando animacion del LifeCounter
     lifeCounterElement.classList.add('heartBeats')
-
-    // Agregando color del estado de salud al lifeCounter
-    // lifeCounterElement.classList.add('lifeCounter--fullHealth')
 
     // Contador de vidas del jugador
     let lifeCounter = 2
@@ -136,16 +84,7 @@ function main() {
             if (numeroJugador !== numeroSecreto && lifeCounter > -1) {
 
                 lifeCounterElement.children[lifeCounter].classList.add('hidden')
-                lifeCounter = lifeCounter - 1
-
-                // Background del lifeCounterElement
-                // if (lifeCounter === 0) {
-                //     lifeCounterElement.classList.add('lifeCounter--lowerHealth')
-                // }
-                // else {
-                //     lifeCounterElement.classList.add('lifeCounter--midHealth')
-                //
-                // }
+                lifeCounter--
 
             }
         }
@@ -156,12 +95,7 @@ function main() {
                 ? alert('You Win Madafaka !!! 😎👉👉')
                 : alert('Game over !!! 🥱')
 
-
             lifeCounter = 2
-
-            // Reiniciando el color de estatus de la vida
-            // lifeCounterElement.classList.remove('lifeCounter--lowerHealth')
-            // lifeCounterElement.classList.add('lifeCounter--fullHealth')
 
             // Reiniciando las vidas
             for (let child of lifeCounterElement.children) {
